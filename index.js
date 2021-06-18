4ff10519ec8c5825dcf2b88aa7f607426d060754
Victor Litvinenko <v@cleanweb.ru>
2021-06-18 13:10:25 +0300

    feat: make server read-only

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
-// const express = require("express");
 const fs = require("fs");
-// const app = express();
-// const server = http.createServer(app);
 const http = require("http");
-const server = http.createServer();
 const { Server } = require("socket.io");
+
+const server = http.createServer();
 const io = new Server(server);
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-let version;
-
 try {
-  version = fs.readFileSync("./version.txt", { encoding: "utf8", flag: "r" }).trim();
-} catch (err) {
-  version = "initial";
-}
+  const version = fs.readFileSync("./version.txt", { encoding: "utf8", flag: "r" }).trim();
+  io.on("connection", (socket) => {
+    console.log("a client connected");
+    io.emit("current version", version);
 
-io.on("connection", (socket) => {
-  console.log("a client connected");
-  io.emit("current version", version);
-
-  socket.on("disconnect", () => {
-    console.log("client disconnected");
+    socket.on("disconnect", () => {
+      console.log("client disconnected");
+    });
   });
 
-  // socket.on("new version", (newVersion) => {
-  //   version = newVersion;
-  //   fs.writeFileSync("./version.txt", version, {
-  //     encoding: "utf8",
-  //     flag: "w",
-  //   });
-  //   io.emit("current version", version);
-  // });
-});
-
-server.listen(5000, () => {
-  console.log("listening on *:5000");
-});
+  server.listen(5000, () => {
+    console.log("listening on *:5000");
+  });
+} catch (err) {
+  console.log('File version.txt not found!');
+}
