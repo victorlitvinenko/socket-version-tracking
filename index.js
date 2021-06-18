70a3f8816bccbda4520e4c4a2c43d920890fb766
Victor Litvinenko <v@cleanweb.ru>
2021-06-18 14:24:50 +0300

    chore: remove extra console logs

--- index.js
+++ index.js
@@ -7,18 +7,8 @@ const io = new Server(server);
 
 try {
   const version = fs.readFileSync("./version.txt", { encoding: "utf8", flag: "r" }).trim();
-  io.on("connection", (socket) => {
-    console.log("a client connected");
-    io.emit("current version", version);
-
-    socket.on("disconnect", () => {
-      console.log("client disconnected");
-    });
-  });
-
-  server.listen(5000, () => {
-    console.log("listening on *:5000");
-  });
+  io.on("connection", () => io.emit("current version", version));
+  server.listen(5000, () => console.log("listening on *:5000"));
 } catch (err) {
   console.log('File version.txt not found!');
 }
